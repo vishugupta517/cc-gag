b771a3a7481c61c0d336e373afb9a9a9a232312f
Prerit07 <54903028+Prerit07@users.noreply.github.com>
2019-09-04 21:44:59 +0530

    Add Name and Rollno
    Name-Prerit, Rollno.-163(cloud computing),F Section

--- name.c
+++ name.c
@@ -1,10 +1,9 @@
 #include<stdio.h>
-
-int main()
+#include<conio.h>
+void main()
 {
+  clrscr();
 printf("CC & GAG\n");
-//enter your name and roll numbers in the print statement
-
-
-return 0;
+printf("Name-Prerit,Rollno-163(Cloud computing)");//enter your name and roll numbers in the print statement
+  getch();
 }
