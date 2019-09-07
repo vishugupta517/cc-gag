6dfd277f4a08e69dc9e318cbdb5be14e907ce98d
Raghav Jagya <raghav23082001@gmail.com>
2019-09-07 18:17:29 +0530

    added Raghav Jagya

--- name.c
+++ name.c
@@ -11,5 +11,6 @@ printf("Divyansh\n1/19/FET/BCS/225");
 printf("Gourisha Goel \n Roll no.- 1/19/FET/BCS/169");
 printf("Y Zakir Fakruddin\n roll no-150");
 printf("Bhavesh Kumar\n Roll no-143");
+printf("Raghav Jagya\nRoll No.=1/19/FET/BCS/221");
 return 0;
 }
