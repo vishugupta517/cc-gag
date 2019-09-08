198aef866fcd4168c0025c8f64814f5f3f92174d
naveedqadir <naveedqadir0@gmail.com>
2019-09-08 20:22:31 +0530

    added my name

--- name.c
+++ name.c
@@ -4,12 +4,6 @@ int main()
 {
 printf("CC & GAG\n");
 //enter your name and roll numbers in the print statement
-printf("Aryan Gaur\n Roll no.144");
-printf("Eashan Sharma\nRoll-140");
-printf("Harsh\nroll-220");
-printf("Divyansh\n1/19/FET/BCS/225");
-printf("Gourisha Goel \n Roll no.- 1/19/FET/BCS/169");
-printf("Y Zakir Fakruddin\n roll no-150");
-printf("Bhavesh Kumar\n Roll no-143");
+printf("Naveed Qadir\n Roll no.1/19/FET/BCS/194");
 return 0;
 }
